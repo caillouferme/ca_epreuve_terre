a164a17b6c17a1415d1ee1da3f71e7d1c8ff9ebb
Pierre Close <55149653+caillouferme@users.noreply.github.com>
2024-01-25 11:32:20 +0100

    Annotation terre00.js
    Annotation pour apprentissage

--- terre00.js
+++ terre00.js
@@ -1,4 +1,7 @@
+// Création d'un tableau à deux éléments
 var alphabet = ["abcdefghijklmnopqrstuvwxyz"," "]
+
+// Règle de la boucle - initialization;condition;increment
 for (var i=0;i<alphabet.length;i++)
 {
 	console.log(alphabet[i])
